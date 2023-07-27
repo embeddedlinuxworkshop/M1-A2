de5744e68c27e3692728fb8e042ca97f0ab44a8d
AhmedAdelWafdy7 <fandes99@gmail.com>
2023-07-27 04:37:12 +0300

    Ahmed Adel A2

--- student_solution.sh
+++ student_solution.sh
@@ -1,1 +1,32 @@
+#!/bin/bash
+
+FILE_PATH="$1"
+
+mkdir -p "$FILE_PATH/txt" "$FILE_PATH/jpg" "$FILE_PATH/pdf" "$FILE_PATH/misc"
+
+for file in "$FILE_PATH"/*; do
+    if [ -f "$file" ]; then
+        filename=$(basename "$file")
+        extension="${filename##*.}"
+
+        case "$extension" in
+            "txt")
+                echo "It's a txt file."
+                mv "$file" "$FILE_PATH/txt/"
+                ;;
+            "jpg")
+                echo "It's a jpg."
+                mv "$file" "$FILE_PATH/jpg/"
+                ;;
+            "pdf")
+                echo "It's a pdf."
+                mv "$file" "$FILE_PATH/pdf/"
+                ;;
+            *)
+                echo "Unknown misc."
+                mv "$file" "$FILE_PATH/misc/"
+                ;;
+        esac
+    fi
+done
 
